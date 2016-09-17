6c88faf942c021a4e2cf8b863ed9802e2f3db979
moznion <moznion@gmail.com>
2016-09-17 10:20:19 +0900

    Reload script

---
+++ tools/reload.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -ex
+
+# echo "Reloading..." | /opt/slackcat --channel 80percent --stream --plain
+
+sudo sysctl -p
+sudo systemctl daemon-reload
+
+# mysql
+sudo /etc/init.d/mysqld restart
+
+# nginx
+sudo /etc/init.d/nginx restart
+
+# middleware
+
+# app
+(cd /home/isucon/webapp/perl && /home/isucon/.local/perl/bin/carton install)
+sudo systemctl restart isuda.per
+sudo systemctl restart isutar.per
+
+# echo "Reloaded!" | /opt/slackcat --channel 80percent --stream --plain
+
